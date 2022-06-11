3ec392888426f2d97cc1a2f4c2490632cc263672
szkolakowskiedu <szymon.kolakowski.education@gmail.com>
2022-06-12 00:30:43 +0200

    auto commit

---
+++ 01_basic_types.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main(){
+	char a;
+	unsigned char b;
+	int c;
+	long d;
+	long double e;
+	float f;
+
+	const int g = 5;
+
+	int tab1[30];
+	char tab2[10];
+
+	struct PointInR2
+	{
+		float x;
+		float y;
+	};
+
+	PointInR2 test_point;
+	test_point.x = 12.0;
+	test_point.y = 24.0;
+
+	int *h;
+
+	return 0;
+}
