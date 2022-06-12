7218dfb7d4640b180a44efb54d54691eff13b43e
szkolakowskiedu <szymon.kolakowski.education@gmail.com>
2022-06-12 12:10:52 +0200

    auto commit

--- 02_basic_operators.cpp
+++ 02_basic_operators.cpp
@@ -4,10 +4,15 @@ int main(){
 	int a, b, c, d;
 	a = b = c = d = 7;
 	int x = (a=3)+(b=c*d);
-	std::cout<<x;
-	return 0;
+	// znak rownosci jest operatorem z priorytetem prawostronnym
+	// x = 52
 
 	int i = 10, j = 10;
 	a = i++; // a = 10, i = 11
 	b = j++; // b = 11, j = 11
+
+	i = j = 1;
+	(i==1 ? a=20 : a=30); // a = 20
+
+	return 0;
 }
