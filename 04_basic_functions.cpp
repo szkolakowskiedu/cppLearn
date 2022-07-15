6adea973b92bc6b3d5f675c534d04bb3e585b305
szkolakowskiedu <szymon.kolakowski.education@gmail.com>
2022-07-15 23:31:40 +0200

    auto commit

--- 04_basic_functions.cpp
+++ 04_basic_functions.cpp
@@ -2,14 +2,22 @@
 
 int func1(int b); // prototyp
 
-int func2(int c){
-	return 2*c;
+int func2(int c){return 2*c;}
+
+void testValues(){
+	int c = 0;
+	static int callCount = 0;
+	c++; callCount++;
 }
 
 int main(){
 	int a,b=3,c=5;
 	a = func1(b)+func2(c)+3;
 	return 0;
+
+	testValues();
+	testValues();
+	// at this point callCount = 2, c = 0
 }
 
 int func1(int b){return b;} // definicja
